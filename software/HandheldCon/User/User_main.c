4fd05fcc2e966b8587ddc4ff3e36542a4f08c9fd
EternalStarCHN <44643190+eternalchn@users.noreply.github.com>
2019-12-12 17:56:39 +0800

    Update
    Update

--- software/HandheldCon/User/User_main.c
+++ software/HandheldCon/User/User_main.c
@@ -17,8 +17,13 @@ void User_main(void)
 	char num[256]={0};
 	while(1)
 	{
+//		HAL_GPIO_WritePin(Led_Red_GPIO_Port,Led_Red_Pin,GPIO_PIN_RESET);
+//		HAL_GPIO_WritePin(Led_Yellow_GPIO_Port,Led_Yellow_Pin,GPIO_PIN_RESET);
+//		HAL_GPIO_WritePin(Led_Blue_GPIO_Port,Led_Blue_Pin,GPIO_PIN_RESET);
+//		HAL_GPIO_WritePin(Led_Green_GPIO_Port,Led_Green_Pin,GPIO_PIN_RESET);
+//		HAL_GPIO_WritePin(Buzzer_GPIO_Port,Buzzer_Pin,GPIO_PIN_RESET);
 		//LCD_Image2LcdDrawBmp565Pic(0, 0,gImage_111);
-		Logic();
+		//Logic();
 	}
 }
 
